11ed8a4be77c9f96462bd5bc6515b5517b403b94
Samuel-Leal-Puentes <andreslepu12@gmail.com>
2026-08-27 07:51:02 -0500

    bloques_acum_y_diff

---
+++ gnuradio/untitled_epy_block_0.py
@@ -0,0 +1,30 @@
+"""
+cosa:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+"""
+
+import numpy as np
+from gnuradio import gr
+
+
+class blk(gr.sync_block):  # other base classes are basic_block, 
+
+    def __init__(self):  # only default arguments here
+    	gr. sync_block . __init__ (
+			self ,
+			name ="e_Diff ", # will show up in GRC
+			in_sig =[ np. float32 ],
+			out_sig =[ np. float32 ]
+)
+		
+def work (self , input_items , output_items ):
+	x = input_items [0] # Senial de entrada .
+	y0 = output_items [0] # Senial acumulada diferencial
+	N = len (x)
+	diff = np. cumsum (x) - self . acum_anterior
+	self . acum_anterior = diff [N -1]
+	y0 [:] = diff
+	return len (y0)
